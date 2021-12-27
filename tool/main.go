078595fb1bbbb24d2e1709a35620b115bdb8b10c
Capulus <baijiu1100@hotmail.com>
2021-12-27 08:22:37 +0000

    update tool/main.go.

--- tool/main.go
+++ tool/main.go
@@ -36,11 +36,36 @@ func main() {
     }
     switch Init.OpType {
         case "precheck":
+            // 检查给定集群是否在dbinfo元数据表中
             err1 := GetMetaClusterNameInfo(Init.ClusterName, Meta.MetaDsn)
             if err1 != nil {
                 return
             }
-            OrchHist, err2 := 
+            // 检查给定集群属于哪个orchestrator集群
+            OrchHost, err2 := orch.GetClusterInwhichOrch(Init.ClusterName,Meta.MetaDsn)
+            if err2 != nil {
+                return
+            }
+            //检查元数据表的的vip和ha_type是否正确
+            if !meta.CheckDbtoolVipAndHaType(Meta.MetaDsn, Init.ClusterName) {
+                return
+            }
+            // 检查及联
+            _, err3 := topology.DetectReplica(Init.ClusterName,OrchHost[0].HostIP, OrchHost[0].BackendPort)
+            if err3 != nil {
+                return
+            }
+            // 检查账号 连通性 权限等
+            infolist, err4 := account.DetectOrchAccount(Init.ClusterName,Meta.MetaDsn)
+            if err4 != nil {
+                return
+            }
+            err5 := access.OrchDiscoverAccountCheck()
+            if err5 != nil {
+                return
+            }
+            
+
         case "switchover":
             OrchHist, _ := orch.GetClusterInwhichOrch(Init.ClusterName, Meta.MetaDsn)
             isCoMas, _ := topology.DetectReplica(Init.ClusterName, OrchHost[0].HostIP, OrchHost[0].BackendPort)
