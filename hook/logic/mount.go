99d4c241abb52304e4a3916c8beabf77ed495e10
Capulus <baijiu1100@hotmail.com>
2021-12-29 09:44:45 +0000

    挂载vip

---
+++ hook/logic/mount.go
@@ -0,0 +1,15 @@
+package logic
+
+import (
+    "fmt"
+    "golang.org/x/crypto/ssh"
+    "github.com/mitchellh/go-homedir"
+    "vfailover/myflag"
+    "time"
+    "io/ioutil"
+    "log"
+)
+
+func MountVip(conf *myflag.OrchCfg, logger *log.Logger) {
+    
+}
