e49073168f74b0500cd632fff9cc40c1e317fe76
Capulus <baijiu1100@hotmail.com>
2021-12-29 09:54:21 +0000

    update hook/logic/mount.go.

--- hook/logic/mount.go
+++ hook/logic/mount.go
@@ -11,5 +11,30 @@ import (
 )
 
 func MountVip(conf *myflag.OrchCfg, logger *log.Logger) {
-    
+    logger.Printf("Begin vip mount on: %v \n", conf.NewMaster)
+    if conf.ClusterType == "kp" || conf.ClusterType == "keepalived" || conf.ClusterType == "orch" {
+        logger.Printf("cluster type: %v \n", conf.ClusterType)
+        sshconn := fmt.Sprintf("%v:22", o.NewMaster)
+        client, err := ssh,Dial("tcp", sshconn, &ssh.ClientConfig{
+            Timeout: time.Seconds,
+            User: o.SSHUser,
+            Auth: []ssh.AuthMethod{publicKeyAuthFunc(o.SSHPublicKeys)},
+            HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+        })
+        if err != nil {
+            return
+        }
+        session, err1 := client.NewSession()
+        if err1 != nil {
+            return
+        }
+        defer session.Close()
+        unionCmd := fmt.Sprintf("%v;%v", conf.CmdVipAdd)
+        mhaKey, err2 := session.CombinedOutput(o.MHAKeyCmd)
+        if err2 != nil {
+            return
+        }
+        MHAKEY := strings.Replace(string(if), "\n", "", -1)
+        o.MHAKey = string(MHAKEY)
+    }
 }
