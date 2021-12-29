9dc09b0bd0683cd6d42781cd73630c2c364abde9
Capulus <baijiu1100@hotmail.com>
2021-12-29 09:42:48 +0000

    kill mysql连接

--- hook/logic/kill.go
+++ hook/logic/kill.go
@@ -28,5 +28,27 @@ func KillMySQLConnection(conf *myflag.OrchCfg, logger *log.Logger) {
     logger.Printf("Being kill old master connection on %v", conf.OldMaster)
     MessSelectSQL := fmt.Sprintf("select id from information_schema.processlist where user not in ('root', 'xxx', 'system user', 'xxx')")
     portInt, _ := strconv.Atoi(conf.OldMasterPort)
-    dsn := fmt.Sprintf("")
+    dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/information_schema", myUser, myPasswd, netWork, conf.OldMaster, portInt)
+    DB, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return
+    }
+    if err4 := DB.Ping(); err4 != nil {
+        logger.Printf("connot connect old master, maybe shutdown or node is down")
+    } else {
+        defer DB.Close()
+        rows, _ := DB.Query(MessSelectSQL)
+        for rows.Next() {
+            err2 := rows.Scan(&d.Id)
+            if err2 != nil {
+                return
+            }
+            // begin kill
+            killsql := fmt.Sprintf("kill %v", d.Id)
+            _, err3 := DB.Exec(killsql)
+            if err3 != nil {
+                logger.Printf("kill failed. error: %v \n", err3)
+            }
+        }
+    }
 }
