ad9ec3b0e452f3d3feec6a855dc7d3b7e712a998
yun <18280188323@163.com>
2022-01-26 23:06:41 +0800

    --调整日志输出

--- go/dealbinlog/mybinlog.go
+++ go/dealbinlog/mybinlog.go
@@ -2,6 +2,7 @@ package dealbinlog
 
 import (
 	"fmt"
+	"log"
 	"os/exec"
 	"strings"
 )
@@ -12,26 +13,31 @@ func DealBinLog(mybinlogpath string, masterip string, masterbinlog string, posit
 	List_num := len(SplitBinlogpath) - 1
 	NewBinLogPathList := SplitBinlogpath[0:List_num]
 	NewBinLogPath := strings.Join(NewBinLogPathList, "/")
-	fmt.Printf("\nThe Binlog Path :%v\n", NewBinLogPath)
-
+	//fmt.Printf("\nThe Binlog Path :%v\n", NewBinLogPath)
+	log.Printf("The Binlog Path :%v\n", NewBinLogPath)
 	//save binlog
 	Getorchleadercmd := "hostname -i "
 	orchleader := execMyCmd(Getorchleadercmd)
-	fmt.Printf("Binlog %v will be scp from %v to orch leader %v\n", masterbinlog, masterip, orchleader)
+	//fmt.Printf("Binlog %v will be scp from %v to orch leader %v\n", masterbinlog, masterip, orchleader)
+	log.Printf("Binlog %v will be scp from %v to orch leader %v\n", masterbinlog, masterip, orchleader)
 
 	savebinlogpath := "/tmp" + masterip + "_" + masterbinlog + "_" + position
 	scp_cmd := "scp mysql@" + masterip + ":" + NewBinLogPath + "/" + masterbinlog + "  " + savebinlogpath
-	fmt.Printf("Save Cmd: %v\n", scp_cmd)
+	//fmt.Printf("Save Cmd: %v\n", scp_cmd)
+	log.Printf("Save Cmd: %v\n", scp_cmd)
 	execMyCmd(scp_cmd)
-	fmt.Printf("Save binlog to %V ok\n", savebinlogpath)
+
+	//fmt.Printf("Save binlog to %V ok\n", savebinlogpath)
+	log.Printf("Save binlog to %V ok\n", savebinlogpath)
 
 	new_parse_binlog := fmt.Sprintf("/tmp/Parsebinlog_%v_%v", masterbinlog, position)
 	MysqlBinlogParse := fmt.Sprintf("mysqlbinlog --start-position=%v --stop-nerver %v > %v", position, savebinlogpath, new_parse_binlog)
 
-	fmt.Printf("MysqlbinlogGet : %v\n", MysqlBinlogParse)
+	//fmt.Printf("MysqlbinlogGet : %v\n", MysqlBinlogParse)
+	log.Printf("MysqlbinlogGet : %v\n", MysqlBinlogParse)
 	execMyCmd(MysqlBinlogParse)
-	fmt.Printf("Parse binlog ok %v\n", new_parse_binlog)
-
+	//fmt.Printf("Parse binlog ok %v\n", new_parse_binlog)
+	log.Printf("Parse binlog ok %v\n", new_parse_binlog)
 	return new_parse_binlog
 }
 
